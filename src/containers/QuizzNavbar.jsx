697a499c2a74d1610921aad6cc00202986e2a394
Maheboob7699 <murdimaheboob.com>
2025-02-22 17:22:45 +0530

    add fucntionality to logout icon

--- src/containers/QuizzNavbar.jsx
+++ src/containers/QuizzNavbar.jsx
@@ -1,14 +1,23 @@
 import '../assets/styles/QuizzNavbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars,  faCircleUser } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom'
 
 function QuizzNavbar() {
+const navigate = useNavigate();
+    const handleLogout=()=>{
+        console.log("logout call");
+        alert("are you sure to logout");
+        navigate('/')
+
+        
+    }
     return (
         <>
             <div className="admin-navbar">
                 <div >
                     <div className="techpatshala-img">
-                        <img src="src/assets/images/techpatshala.svg" alt="" />
+                        <img src="/src/assets/images/techpatshala.svg" alt="" />
                     </div>
                     <div className="side-bar">
                         <FontAwesomeIcon icon={faBars} />
@@ -18,7 +27,7 @@ function QuizzNavbar() {
 
                 <div className="admin-section">
                     <p>admin</p>
-                    <div className="admin-logout">
+                    <div className="admin-logout" onClick={handleLogout}>
                     <FontAwesomeIcon icon={ faCircleUser} />
                     </div>
                 </div>
